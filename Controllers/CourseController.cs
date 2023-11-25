a5bb13e7dd717e091401472a30c115c143b84218
kodbutonu <oergin526@gmail.com>
2023-11-26 00:07:48 +0300

    İmage eklendi

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -3,18 +3,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace basics.Controllers;
 
-public class CourseController:Controller{
+public class CourseController : Controller
+{
 
-public IActionResult Index(){
-    var kurs=new Course();
-    kurs.Id=1;
-    kurs.Title="Asp.NET";
-    kurs.Description="Asp.NET using backend developing in web technology.";
-   return View(kurs);
-}
+    public IActionResult Index()
+    {
+        var kurs = new Course();
+        kurs.Id = 1;
+        kurs.Title = "Asp.NET";
+        kurs.Description = "Asp.NET using backend developing in web technology.";
+        return View(kurs);
+    }
     public IActionResult List()
     {
-
-        return View();
+        var kurslar = new List<Course>()
+       {
+           new Course() { Id = 1, Title = "Asp.Net", Description = "Asp.NET using backend developing in web technology.",Image="1.jpg"},
+           new Course() { Id = 2, Title = "JavaScript", Description = "JavaScript ile frontend geliştirmeye giriş.",Image="2.jpg"},
+new Course() { Id = 3, Title = "C# Programming", Description = "C# programlama dilinin temelleri." ,Image="3.jpg"}
+       };
+        return View("List",kurslar);
     }
 }
